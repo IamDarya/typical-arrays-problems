4d717ae8ae12c324b02899b82c644b67650e7d52
IamDarya <daria.vyacheslavovna@gmail.com>
2021-03-05 00:09:33 +0100

    init: fix typical array problem
    solution of  task "Typical Arrays Problems"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(array === undefined || array.length === 0) {
+		return 0;
+	}
+	let result = Number.MAX_VALUE;
+	for (let i = 0; i < array.length; i++){
+		if (array[i] <= result) {
+			let save = array[i]; 
+            result = save;
+        }
+    }
+return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length === 0) {
+        return 0;
+    }
+    let result = Number.MIN_VALUE;
+    for (let i = 0; i < array.length; i++){
+        if (array[i] >= result) {
+            let save = array[i];     
+            result = save;
+        }
+    }
+return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length === 0) {
+        return 0;
+    }
+    let save = 0;
+    for (let i = 0; i < array.length; i++){
+    	save = save + array[i];
+    }
+    return save/array.length;
 }
